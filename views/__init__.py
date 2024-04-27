eafcb1ab1c6db21d81b0a8f602c18621db4da8a7
adirizq <rizkyzadadi@gmail.com>
2024-04-27 22:36:48 +0700

    Migrate prediction form post to ajax post

--- views/__init__.py
+++ views/__init__.py
@@ -1,5 +1,6 @@
 from .auth import auth
 from .dashboard import dashboard
 from .qa_pack import qa_pack
+from .inference import inference
 
-views = [auth, dashboard, qa_pack]
+views = [auth, dashboard, qa_pack, inference]
